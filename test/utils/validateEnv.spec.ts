accf09ec2747db9e57ed9cd312dfae6c85735282
Naomi Carrigan <nhcarrigan@gmail.com>
2023-08-22 20:30:16 -0700

    chore: tests

--- test/utils/validateEnv.spec.ts
+++ test/utils/validateEnv.spec.ts
@@ -26,14 +26,15 @@ suite("validateEnv utility", () => {
     assert.throws(validateEnv, "Missing TICKET_LOG_HOOK environment variable");
   });
 
-  test("throws an error when missing MONGO_URL", () => {
+  test("throws an error when missing MONGO_URI", () => {
     process.env.TICKET_LOG_HOOK =
       // This is not a live webhook URL, so don't bother trying to use it.
       "https://canary.discord.com/api/webhooks/1133857667505463326/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-    assert.throws(validateEnv, "Missing MONGO_URL environment variable");
+    assert.throws(validateEnv, "Missing MONGO_URI environment variable");
   });
 
   test("returns the environment cache when all variables are present", () => {
+    process.env.MONGO_URI = "hi";
     const result = validateEnv();
     assert.equal(result.token, "discord bot token");
     assert.equal(result.homeGuild, "123");
