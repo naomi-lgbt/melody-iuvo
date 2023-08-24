f2b1cd6ced1e80a86fee9f3230f77c0dbfce64fc
Naomi Carrigan <nhcarrigan@gmail.com>
2023-08-24 11:58:09 -0700

    feat: build commands

---
+++ src/modules/subcommands/plural/handlePluralFront.ts
@@ -0,0 +1,55 @@
+import { CommandHandler } from "../../../interfaces/CommandHandler";
+import { errorHandler } from "../../../utils/errorHandler";
+import { getDatabaseRecord } from "../../../utils/getDatabaseRecord";
+
+/**
+ * Sets auto proxying for an identity.
+ */
+export const handlePluralFront: CommandHandler = async (bot, interaction) => {
+  try {
+    const name = interaction.options.getString("name");
+    if (!name) {
+      await bot.db.users.update({
+        where: {
+          userId: interaction.user.id,
+        },
+        data: {
+          front: "",
+        },
+      });
+      await interaction.editReply({
+        content: "I have disabled auto-proxying for you.",
+      });
+      return;
+    }
+    const record = await getDatabaseRecord(bot, interaction.user.id);
+    const exists = record.plurals.find((plural) => plural.name === name);
+
+    if (!exists) {
+      await interaction.editReply({
+        content:
+          "Please forgive me, but you do not have an identity with that name.",
+      });
+      return;
+    }
+
+    await bot.db.users.update({
+      where: {
+        userId: interaction.user.id,
+      },
+      data: {
+        front: name,
+      },
+    });
+
+    await interaction.editReply({
+      content: `I will now auto-proxy your messages as ${name}.`,
+    });
+  } catch (err) {
+    await errorHandler(bot, "plural delete command", err);
+    await interaction.editReply({
+      content:
+        "Forgive me, but I failed to complete your request. Please try again later.",
+    });
+  }
+};
