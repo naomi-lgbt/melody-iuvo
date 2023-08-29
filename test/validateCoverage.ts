e5c7f388a7e0ee006f426524cbfe359b7886d227
Naomi Carrigan <nhcarrigan@gmail.com>
2023-08-28 17:09:41 -0700

    feat: confirm that all src files appear in the coverage report
    This means, at the very least, they have been used somewhere in the
    test suite, so we know coverage is being validated.

--- test/validateCoverage.ts
+++ test/validateCoverage.ts
@@ -1,4 +1,4 @@
-import { readdir, stat } from "fs/promises";
+import { readFile, readdir, stat } from "fs/promises";
 import { join } from "path";
 
 import { logHandler } from "../src/utils/logHandler";
@@ -18,20 +18,45 @@ const loadDirectory = async (path: string): Promise<string[]> => {
 };
 
 (async () => {
+  const checkNycOutput = process.argv.includes("--post-coverage");
   const src = await loadDirectory(join(process.cwd(), "src"));
   const test = await loadDirectory(join(process.cwd(), "test"));
-  const filesUntested = src.filter(
+  if (!checkNycOutput) {
+    const filesUntested = src.filter(
+      (file) =>
+        // exclude files in the src/interface directory as types do not need testing
+        !/src\/interface/.test(file) &&
+        !test.includes(file.replace("src", "test").replace(".ts", ".spec.ts"))
+    );
+    if (filesUntested.length) {
+      logHandler.error(
+        `The following files do not have a corresponding spec:\n${filesUntested.join(
+          "\n"
+        )}`
+      );
+      process.exit(1);
+    }
+    logHandler.info("All files have a corresponding spec.");
+    return;
+  }
+  const nycProcessData = await readFile(
+    join(process.cwd(), ".nyc_output", "processinfo", "index.json"),
+    "utf-8"
+  );
+  const nycProcessInfo = JSON.parse(nycProcessData);
+  const coveredFileList = Object.keys(nycProcessInfo.files);
+  const nonCoveredFiles = src.filter(
     (file) =>
       // exclude files in the src/interface directory as types do not need testing
-      !/src\/interface/.test(file) &&
-      !test.includes(file.replace("src", "test").replace(".ts", ".spec.ts"))
+      !/src\/interface/.test(file) && !coveredFileList.includes(file)
   );
-  if (filesUntested.length) {
+  if (nonCoveredFiles.length) {
     logHandler.error(
-      `The following files do not have a corresponding spec:\n${filesUntested.join(
+      `The following files were not found in the coverage report:\n${nonCoveredFiles.join(
         "\n"
       )}`
     );
     process.exit(1);
   }
+  logHandler.info("All files are covered by the coverage report.");
 })();
