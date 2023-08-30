864b07fcf60e833cf4aab16b4933aa80f10066b2
Naomi Carrigan <nhcarrigan@gmail.com>
2023-08-29 21:48:32 -0700

    chore: make random value test less flaky

--- test/utils/getRandomValue.spec.ts
+++ test/utils/getRandomValue.spec.ts
@@ -9,10 +9,10 @@ suite("getRandomValue", () => {
       const random = getRandomValue([1, 2, 3, 4, 5]);
       counts[random] = counts[random] ? counts[random] + 1 : 1;
     }
-    assert.approximately(counts[1], 200, 25);
-    assert.approximately(counts[2], 200, 25);
-    assert.approximately(counts[3], 200, 25);
-    assert.approximately(counts[4], 200, 25);
-    assert.approximately(counts[5], 200, 25);
+    assert.approximately(counts[1], 200, 30);
+    assert.approximately(counts[2], 200, 30);
+    assert.approximately(counts[3], 200, 30);
+    assert.approximately(counts[4], 200, 30);
+    assert.approximately(counts[5], 200, 30);
   });
 });
