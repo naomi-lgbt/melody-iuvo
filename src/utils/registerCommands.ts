920083e076888a9b2738995a8a8f65a5231ad840
Naomi Carrigan <nhcarrigan@gmail.com>
2023-08-30 12:54:34 -0700

    feat(tools): finish testing utils

--- src/utils/registerCommands.ts
+++ src/utils/registerCommands.ts
@@ -8,20 +8,27 @@ import { errorHandler } from "./errorHandler";
  * Registers the loaded commands to Discord.
  *
  * @param {ExtendedClient} bot The bot's Discord instance.
+ * @param {REST} restClass A mock REST client for testing.
+ * @returns {Promise<REST>} The REST client, only for testing.
  */
-export const registerCommands = async (bot: ExtendedClient) => {
+export const registerCommands = async (
+  bot: ExtendedClient,
+  restClass = REST
+): Promise<REST | null> => {
   try {
     if (!bot.user) {
       throw new Error("Bot is not logged in. Cannot register commands yet.");
     }
-    const rest = new REST({ version: "10" }).setToken(bot.env.token);
+    const rest = new restClass({ version: "10" }).setToken(bot.env.token);
     const commands = bot.commands.map((c) => c.data.toJSON());
 
     await rest.put(
       Routes.applicationGuildCommands(bot.user.id, bot.env.homeGuild),
       { body: commands }
     );
+    return rest;
   } catch (err) {
     await errorHandler(bot, "register commands utility", err);
+    return null;
   }
 };
