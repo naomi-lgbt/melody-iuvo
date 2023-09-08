0485aae589c478c09222c91aeae441b011060007
Naomi Carrigan <nhcarrigan@gmail.com>
2023-09-08 16:20:43 -0700

    chore: prepare response config

---
+++ src/config/Responses.ts
@@ -0,0 +1,11 @@
+export const Responses: {
+  [type: string]: {
+    [userId: string]: string;
+  };
+} = {
+  melodyPing: {
+    "478752726612967435": "Good day, my Queen. What might you require?",
+    "465650873650118659": "Hello, Mistress. Might I be of assistance?",
+    "710195136700874893": "Hello, Mistress. Might I be of assistance?",
+  },
+};
