0473d76885236f455aeeecc4b0d53e20afbf7719
Naomi Carrigan <nhcarrigan@gmail.com>
2023-09-08 16:34:47 -0700

    fix(tools): test that keys are correct length

--- test/config/Responses.spec.ts
+++ test/config/Responses.spec.ts
@@ -8,6 +8,11 @@ suite("Responses", () => {
   const keys = Object.keys(Responses);
   test("should have a response for every expected ID", () => {
     for (const key of keys) {
+      assert.lengthOf(
+        Object.keys(Responses[key]),
+        ids.length,
+        `${key} does not have the correct number of ids`
+      );
       for (const id of ids) {
         assert.property(
           Responses[key],
