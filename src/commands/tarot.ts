21c8bde07fffd76c64dd0e6dd6d88016ae735834
Naomi Carrigan <nhcarrigan@gmail.com>
2023-09-29 21:15:40 -0700

    fix: use real random numbers for shuffle algorithm

--- src/commands/tarot.ts
+++ src/commands/tarot.ts
@@ -7,7 +7,7 @@ import { errorHandler } from "../utils/errorHandler";
 
 const shuffleArray = <T>(array: T[]): T[] => {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = rand.uniformInt(0, i)();
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
