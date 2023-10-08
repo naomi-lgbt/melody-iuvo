b0d23cf62fab29fec6cfb151108e0d32347dbcc3
Naomi Carrigan <nhcarrigan@gmail.com>
2023-10-08 12:44:50 -0700

    feat: have melody handle congrats messages

---
+++ src/config/Github.ts
@@ -0,0 +1,23 @@
+export const IgnoredActors = [
+  "renovate[bot]",
+  "codeclimate[bot]",
+  "dependabot[bot]",
+  "lgtm-com[bot]",
+  "deepsource-autofix[bot]",
+  "sonarcloud[bot]",
+  "naomi-lgbt",
+];
+
+export const ThankYou = `## Thank You
+
+Thank you for your continued contribution to our project. We have reviewed your pull request and are happy to accept these changes.
+
+Please continue to watch for issues labelled \`help wanted\`, as these will be additional opportunities to contribute.
+
+You can also see all open issues [through our contributor tool](https://contribute.naomi.lgbt). Also, feel free to join our [Discord server](https://chat.naomi.lgbt) to chat with us and get notified when new issues are available!`;
+
+export const FirstTimer = `## Congratulations
+
+Congratulations on making your first pull request to our project. We are excited to review your changes, and thank you for your patience.
+
+If you have any questions, please feel free to ask us in our [Discord server](https://chat.naomi.lgbt).`;
