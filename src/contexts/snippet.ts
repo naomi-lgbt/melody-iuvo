206b584aa81255d9744de2df8498f451a4696042
Naomi Carrigan <nhcarrigan@gmail.com>
2023-10-23 09:08:56 -0700

    feat: complete snippet command logic

--- src/contexts/snippet.ts
+++ src/contexts/snippet.ts
@@ -1,3 +1,11 @@
+import {
+  ActionRowBuilder,
+  ComponentType,
+  Message,
+  StringSelectMenuBuilder,
+} from "discord.js";
+
+import { Snippets } from "../config/Snippets";
 import { Context } from "../interfaces/Context";
 import { errorHandler } from "../utils/errorHandler";
 import { isOwner } from "../utils/isOwner";
@@ -16,7 +24,65 @@ export const snippet: Context = {
         });
         return;
       }
-      await interaction.editReply({ content: "Hi Mama!" });
+      const message = interaction.options.getMessage(
+        "message",
+        true
+      ) as Message;
+
+      const dropdown = new StringSelectMenuBuilder()
+        .setCustomId("snippets")
+        .addOptions(
+          ...Snippets.map(({ name }) => ({
+            label: name,
+            value: name,
+          }))
+        )
+        .setMaxValues(1)
+        .setMinValues(1);
+      const row = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+        dropdown
+      );
+
+      const response = await interaction.editReply({
+        content: "Which snippet would you like to send?",
+        components: [row],
+      });
+
+      const collector =
+        response.createMessageComponentCollector<ComponentType.StringSelect>({
+          time: 1000 * 60 * 60,
+        });
+
+      collector.on("collect", async (selection) => {
+        await selection.deferUpdate();
+        const name = selection.values[0];
+        const target = Snippets.find((s) => s.name === name);
+        if (!target) {
+          await selection.editReply({
+            content: `Cannot find a snippet with the name ${name}.`,
+          });
+          return;
+        }
+        await message.reply({
+          content: target.response,
+        });
+        await interaction.editReply({
+          content: "Response sent!",
+          components: [],
+        });
+      });
+
+      collector.on("end", async () => {
+        /**
+         * Ephemerals can be deleted. We catch the error here because we don't really
+         * care if it fails.
+         */
+        await interaction
+          .editReply({
+            components: [],
+          })
+          .catch(() => null);
+      });
     } catch (err) {
       await errorHandler(bot, "snippet context", err);
     }
