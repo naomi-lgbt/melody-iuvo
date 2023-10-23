9b0c42241fa752724e7532f5274fffb65e8c06b7
Naomi Carrigan <nhcarrigan@gmail.com>
2023-10-23 09:40:03 -0700

    feat: don't do autoresponse in heavy discussion channels (#137)

--- src/interfaces/GuildMessage.ts
+++ src/interfaces/GuildMessage.ts
@@ -1,6 +1,7 @@
-import { Guild, GuildMember, Message } from "discord.js";
+import { Guild, GuildMember, GuildTextBasedChannel, Message } from "discord.js";
 
 export interface GuildMessage extends Message {
   guild: Guild;
   member: GuildMember;
+  channel: GuildTextBasedChannel;
 }
