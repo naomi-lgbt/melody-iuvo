d5ffa169d611e11156c0ed4e11e7fca043ca9cfb
Naomi Carrigan <nhcarrigan@gmail.com>
2023-11-02 12:33:59 -0700

    feat(tools): set up knip (#158)
    * feat(tools): set up knip
    
    * chore: resolve knip errors
    
    * feat(tools): add knip to workflows
    
    * fix(tools): resolve broken tests

--- test/config/BirthdayGifs.spec.ts
+++ test/config/BirthdayGifs.spec.ts
@@ -1,20 +1,15 @@
 import { assert } from "chai";
 
-import { CryingGifs, BirthdayGifs } from "../../src/config/BirthdayGifs";
+import { BirthdayGifs } from "../../src/config/BirthdayGifs";
 
 suite("Birthday gifs", () => {
-  test("should have unique crying gifs", () => {
-    const set = new Set(CryingGifs);
-    assert.strictEqual(set.size, CryingGifs.length);
-  });
-
   test("should have unique birthday gifs", () => {
     const set = new Set(BirthdayGifs);
     assert.strictEqual(set.size, BirthdayGifs.length);
   });
 
   test("should all be gif urls", () => {
-    const combined = [...CryingGifs, ...BirthdayGifs];
+    const combined = [...BirthdayGifs];
     for (const gif of combined) {
       assert.match(gif, /\.gif$/, `${gif} is not a GIF URL`);
     }
