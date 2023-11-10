f5fb09fb30f98026d1dbdac05a88991444e3f260
Naomi Carrigan <nhcarrigan@gmail.com>
2023-11-10 08:52:23 -0800

    feat: add bubbles command

---
+++ src/commands/bubbles.ts
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder } from "discord.js";
+import rand from "random";
+
+import { Command } from "../interfaces/Command";
+import { errorHandler } from "../utils/errorHandler";
+
+export const bubbles: Command = {
+  data: new SlashCommandBuilder()
+    .setName("bubbles")
+    .setDescription(
+      "Generate a sheet of bubble wrap to pop. Make sure you don't have spoilers turned off!"
+    )
+    .setDMPermission(false),
+  run: async (bot, interaction) => {
+    try {
+      await interaction.deferReply();
+      const randomGenerator = rand.uniformInt(10, 20);
+      const rows = randomGenerator();
+      const columns = randomGenerator();
+      const bubbleWrap = Array.from({ length: rows }, () =>
+        "||<a:bubble:1172567849550745720>||".repeat(columns)
+      ).join("\n");
+      await interaction.editReply({
+        content: `Please enjoy this sheet of bubble wrap.\n${bubbleWrap}`
+      });
+    } catch (err) {
+      await errorHandler(bot, "bubbles command", err);
+    }
+  }
+};
