357f111c1c97a46816ce79f0dc83bb998e5f4d8b
Naomi Carrigan <nhcarrigan@gmail.com>
2024-01-04 17:30:25 -0800

    fix: import

--- test/commands/reload-jobs.spec.ts
+++ test/commands/reload-jobs.spec.ts
@@ -1,6 +1,6 @@
 import { assert } from "chai";
 
-import { reloadJobs } from "../../src/commands/reloadJobs";
+import { reloadJobs } from "../../src/commands/reload-jobs";
 
 suite("reloadJobs command", () => {
   test("is defined", () => {
