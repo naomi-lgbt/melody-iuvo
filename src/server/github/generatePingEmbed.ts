d147c8f9bef5f3964da9d1e9053d15edfcea35da
Naomi Carrigan <nhcarrigan@gmail.com>
2024-02-13 17:33:33 -0800

    fix: reduce github noise

--- src/server/github/generatePingEmbed.ts
+++ src/server/github/generatePingEmbed.ts
@@ -1,30 +1,5 @@
-import { APIEmbed } from "discord.js";
-
 import { GithubPingPayload } from "../../interfaces/GitHubPayloads";
 
-export const generatePingEmbed = (data: GithubPingPayload): APIEmbed => {
-  return {
-    title: "Naomi has a new project!",
-    url: data.repository.html_url,
-    color: 0x8b4283,
-    description: `I will now watch for activity on the \`${data.repository.name}\` project.`,
-    author: {
-      name: data.sender.login || "unknown",
-      icon_url:
-        data.sender.avatar_url || "https://cdn.nhcarrigan.com/profile.png"
-    },
-    fields: [
-      {
-        name: "Owner",
-        value: data.repository.owner.login || "Unknown."
-      },
-      {
-        name: "Description",
-        value: data.repository.description || "None provided."
-      }
-    ],
-    footer: {
-      text: data.zen
-    }
-  };
+export const generatePingEmbed = (data: GithubPingPayload): string => {
+  return `[Now watching ${data.repository.name}](<${data.repository.url}>)`;
 };
--- src/server/github/generateStarEmbed.ts
+++ src/server/github/generateStarEmbed.ts
@@ -1,33 +1,8 @@
-import { APIEmbed } from "discord.js";
-
 import { GithubStarPayload } from "../../interfaces/GitHubPayloads";
 
-export const generateStarEmbed = (data: GithubStarPayload): APIEmbed => {
-  return {
-    title: "Naomi got a star!",
-    url: data.repository.html_url,
-    color: 0x8b4283,
-    description: `The following repository has ${
-      data.action === "created" ? "gained" : "lost"
-    } a star.`,
-    author: {
-      name: data.sender.login || "unknown",
-      icon_url:
-        data.sender.avatar_url ||
-        "https://cdn.nhcarrigan.com/content/profile.jpg"
-    },
-    fields: [
-      {
-        name: "Repository",
-        value: data.repository.name || "unknown"
-      },
-      {
-        name: "Owner",
-        value: data.repository.owner.login || "unknown"
-      }
-    ],
-    footer: {
-      text: `This repository now has ${data.repository.stargazers_count} stars.`
-    }
-  };
+export const generateStarEmbed = (data: GithubStarPayload): string | null => {
+  if (data.action !== "created") {
+    return null;
+  }
+  return `[New stargazer! ${data.repository.name}](<${data.repository.html_url}>)`;
 };
