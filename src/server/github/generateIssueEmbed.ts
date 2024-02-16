366916d0c78571ca95cb4741a59069ec285d0c99
Mama Naomi <nhcarrigan@gmail.com>
2024-02-15 22:15:26 -0800

    feat: improve the github logic (#353)

--- src/server/github/generateIssueEmbed.ts
+++ src/server/github/generateIssueEmbed.ts
@@ -3,8 +3,23 @@ import { GithubIssuesPayload } from "../../interfaces/GitHubPayloads";
 export const generateIssuesEmbed = (
   data: GithubIssuesPayload
 ): string | null => {
-  if (!["opened", "edited", "deleted", "closed"].includes(data.action)) {
+  if (!["opened", "edited", "closed"].includes(data.action)) {
     return null;
+  }
+  if (data.action === "closed") {
+    if (data.issue.state_reason === "completed") {
+      return `[Issue closed as complete - ${data.repository.name}#${data.issue.number}](<${data.issue.html_url}>)`;
+    }
+    if (data.issue.state_reason === "not_planned") {
+      return `[Issue closed as not planned - ${data.repository.name}#${data.issue.number}](<${data.issue.html_url}>)`;
+    }
+    return `[Issue closed - ${data.repository.name}#${data.issue.number}](<${data.issue.html_url}>)`;
+  }
+  if (data.issue.state_reason === "reopened") {
+    return `[Issue reopened - ${data.repository.name}#${data.issue.number}](<${data.issue.html_url}>)`;
+  }
+  if (data.action === "edited") {
+    return `[Issue updated - ${data.repository.name}#${data.issue.number}](<${data.issue.html_url}>)`;
   }
   return `[New issue created - ${data.repository.name}#${data.issue.number}](<${data.issue.html_url}>)`;
 };
--- src/server/github/generatePullEmbed.ts
+++ src/server/github/generatePullEmbed.ts
@@ -7,5 +7,11 @@ export const generatePullEmbed = (data: GithubPullPayload): string | null => {
   if (data.pull_request.merged) {
     return `[Pull request merged - ${data.repository.name}#${data.pull_request.number}](<${data.pull_request.html_url}>)`;
   }
+  if (data.action === "edited") {
+    return `[Pull request updated - ${data.repository.name}#${data.pull_request.number}](<${data.pull_request.html_url}>)`;
+  }
+  if (data.action === "closed") {
+    return `[Pull request closed - ${data.repository.name}#${data.pull_request.number}](<${data.pull_request.html_url}>)`;
+  }
   return `[New pull request - ${data.repository.name}#${data.pull_request.number}](<${data.pull_request.html_url}>)`;
 };
